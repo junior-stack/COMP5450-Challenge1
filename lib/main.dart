d7df47891f0776d0d2413a5960a59c35669c4f6b
korosuke <korosuke@gmail.com>
2025-05-12 16:04:07 -0400

    login_pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:challenge1/pages/LoginPage.dart';
 import 'package:flutter/material.dart';
 import 'package:challenge1/pages/ChatPage.dart';
 
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const ChatPage(title: 'Flutter Demo Home Page'),
+      home: const LoginPage(),
     );
   }
 }
